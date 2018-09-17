5cd9247c118ddf4bed1cf67e1d883bcd6a50550d
Marcial <a15110051@gmail.com>
2018-09-17 12:57:44 -0500

    Agregar archivos de proyecto.

---
+++ DutchTreat/Controller/AppController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DutchTreat.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace DutchTreat.Controllers
+{
+    public class AppController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet("Contact")]
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        [HttpPost("Contact")]
+        public IActionResult Contact(ContactViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                //send email
+                _mailService.SendMail("marcialcga@gmail.com", model.Subject, $"From: {model.Name} - {model.Email}, Message: {model.Message}");
+            }
+            return View();
+        }
+
+        [HttpGet("About")]
+        public IActionResult About()
+        {
+            return View();
+        }
+    }
+}
---
+++ DutchTreat/wwwroot/index.js
@@ -0,0 +1,31 @@
+﻿$(document).ready(function () { 
+    console.log("Hola Mundo!");
+
+    //var theForm = document.getElementById("the-form");
+    //theForm.hidden = true;
+    var theform = $("#the-form");
+    theform.hide();
+
+
+    //var button = document.getElementById("buyButton");
+    //button.addEventListener("click", function () {
+    //    console.log("Buying item");
+    //});
+    var button = $("#buyButton");
+    button.on("click", function () {
+        console.log("Buying item");
+    });
+
+    var productInfo = $(".product-props li");
+    productInfo.on("click", function () {
+        console.log("You clicked on " + $(this).text()); 
+    });
+
+    var $loginToggle = $("#loginToggle");
+    var $popupForm = $(".popup-form");
+
+    $loginToggle.on("click", function () {
+        $popupForm.fadeToggle(1000);
+        //$popupForm.slideToggle(1000);
+    });
+});
